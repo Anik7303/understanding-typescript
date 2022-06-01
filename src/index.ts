8f11f32ba99d874400bb29bcc00230e5bc427085
Anik Mohammad <anik7303@gmail.com>
2022-06-01 22:25:08 +0600

    using lodash with @types/lodash

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-import { ProjectStatus } from "./models/project";
+import _ from "lodash";
 
-new ProjectInput();
-new ProjectList(ProjectStatus.Active);
-new ProjectList(ProjectStatus.Finished);
+console.log(_.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
